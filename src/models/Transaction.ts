8bc8fbb38b6f5dfb486e505a86a2909b270cdd29
Ivan Seibel <ivanseibel@gmail.com>
2020-06-28 09:42:36 -0300

    Correct provider to avatar relation name

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -26,7 +26,7 @@ class Transaction {
 
   @ManyToOne(() => Category)
   @JoinColumn({ name: 'category_id' })
-  provider: Category;
+  category: Category;
 
   @Column('timestamp')
   created_at: Date;
