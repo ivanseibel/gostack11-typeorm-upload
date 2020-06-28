90e83c2c5365a4bf69baa661f096733a363d71d5
Ivan Seibel <ivanseibel@gmail.com>
2020-06-28 09:47:30 -0300

    Implement CreateTransactionService

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,8 @@
 // import AppError from '../errors/AppError';
 
+import { getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface Request {
   title: string;
@@ -16,7 +18,38 @@ class CreateTransactionService {
     type,
     category,
   }: Request): Promise<Transaction> {
-    // TODO
+    const categoriesRepository = getRepository(Category);
+
+    const categoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    let category_id: string;
+
+    if (!categoryExists) {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+
+      category_id = newCategory.id;
+    } else {
+      category_id = categoryExists.id;
+    }
+
+    const transactionsRepository = getRepository(Transaction);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
