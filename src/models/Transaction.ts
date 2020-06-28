00bb9e576536415da2f6639e0c9108724a1175bf
Ivan Seibel <ivanseibel@gmail.com>
2020-06-28 09:25:44 -0300

    Create transactions/category models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,37 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   title: string;
 
+  @Column('varchar')
   type: 'income' | 'outcome';
 
+  @Column('numeric')
   value: number;
 
+  @Column('varchar')
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  provider: Category;
+
+  @Column('timestamp')
   created_at: Date;
 
+  @Column('timestamp')
   updated_at: Date;
 }
 
